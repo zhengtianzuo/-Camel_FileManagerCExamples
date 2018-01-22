13819005cc763b05d7839722db0ede0cff4e0802
zhengtianzuo <camelsoft@163.com>
2018-01-22 17:24:05 +0800

    update

--- CamelFileManagerCTest/src/CamelFileManagerCTest.cpp
+++ CamelFileManagerCTest/src/CamelFileManagerCTest.cpp
@@ -6,7 +6,7 @@
  *@author zhengtianzuo
 */
 #include "CamelFileManagerCTest.h"
-
+#include <QDateTime>
 
 CamelFileManagerCTest::CamelFileManagerCTest()
 {
@@ -150,6 +150,93 @@ void CamelFileManagerCTestThread::Sub_subDataTest()
         Cls_stuGetUserData getUserData(reinterpret_cast<void*&>(intInt), intSize);
         intError = Sub_FMInt->Cls_funManagerData_Extract(&dBVerify, &dataType, &fun, &getUserData);
         emit addInfo(QString("Cls_funManagerData_Extract"), QStringLiteral(" 获取整型数据"), QString("1122"), QString::number(intInt), (intError==1)?QStringLiteral("成功"):QStringLiteral("错误!!!"));
+    }
+
+    {
+        //加入浮点型数据
+        float fltFloat = 1234.56;
+        Cls_stuDataType dataType(clsFileManager_intDataType_Float, -1, "float");
+        Cls_stuUserData userData(&fltFloat, 0);
+        intError = Sub_FMInt->Cls_funManagerData_Combine(&dBVerify, &dataType, nullptr, &userData, false, -1);
+        emit addInfo(QString("Cls_funManagerData_Combine"), QStringLiteral(" 加入浮点型数据"), QString("1"), QString::number(intError), (intError==1)?QStringLiteral("成功"):QStringLiteral("错误!!!"));
+
+        //修改浮点型数据
+        fltFloat = 1122.33;
+        intError = Sub_FMInt->Cls_funManagerData_Modify(&dBVerify, &dataType, &userData, false);
+        emit addInfo(QString("Cls_funManagerData_Modify"), QStringLiteral(" 修改浮点型数据"), QString("1"), QString::number(intError), (intError==1)?QStringLiteral("成功"):QStringLiteral("错误!!!"));
+
+        //获取浮点型数据
+        fltFloat = 0.0;
+        intSize = 0;
+        Cls_stuGetUserData getUserData(reinterpret_cast<void*&>(fltFloat), intSize);
+        intError = Sub_FMInt->Cls_funManagerData_Extract(&dBVerify, &dataType, &fun, &getUserData);
+        emit addInfo(QString("Cls_funManagerData_Extract"), QStringLiteral(" 获取浮点型数据"), QString("1122.33"), QString::number(fltFloat), (intError==1)?QStringLiteral("成功"):QStringLiteral("错误!!!"));
+    }
+
+    {
+        //加入双浮点型数据
+        double dblDouble = 1234.56;
+        Cls_stuDataType dataType(clsFileManager_intDataType_Double, -1, "double");
+        Cls_stuUserData userData(&dblDouble, 0);
+        intError = Sub_FMInt->Cls_funManagerData_Combine(&dBVerify, &dataType, nullptr, &userData, false, -1);
+        emit addInfo(QString("Cls_funManagerData_Combine"), QStringLiteral(" 加入双浮点型数据"), QString("1"), QString::number(intError), (intError==1)?QStringLiteral("成功"):QStringLiteral("错误!!!"));
+
+        //修改双浮点型数据
+        dblDouble = 1122.33;
+        intError = Sub_FMInt->Cls_funManagerData_Modify(&dBVerify, &dataType, &userData, false);
+        emit addInfo(QString("Cls_funManagerData_Modify"), QStringLiteral(" 修改双浮点型数据"), QString("1"), QString::number(intError), (intError==1)?QStringLiteral("成功"):QStringLiteral("错误!!!"));
+
+        //获取双浮点型数据
+        dblDouble = 0.0;
+        intSize = 0;
+        Cls_stuGetUserData getUserData(reinterpret_cast<void*&>(dblDouble), intSize);
+        intError = Sub_FMInt->Cls_funManagerData_Extract(&dBVerify, &dataType, &fun, &getUserData);
+        emit addInfo(QString("Cls_funManagerData_Extract"), QStringLiteral(" 获取双浮点型数据"), QString("1122.33"), QString::number(dblDouble), (intError==1)?QStringLiteral("成功"):QStringLiteral("错误!!!"));
+    }
+
+    {
+        //加入日期型数据
+        double dateTime = QDateTime::currentDateTime().toMSecsSinceEpoch();
+        Cls_stuDataType dataType(clsFileManager_intDataType_Date, -1, "datetime");
+        Cls_stuUserData userData(&dateTime, 0);
+        intError = Sub_FMInt->Cls_funManagerData_Combine(&dBVerify, &dataType, nullptr, &userData, false, -1);
+        emit addInfo(QString("Cls_funManagerData_Combine"), QStringLiteral(" 加入日期型数据"), QString("1"), QString::number(intError), (intError==1)?QStringLiteral("成功"):QStringLiteral("错误!!!"));
+
+        //修改日期型数据
+        dateTime = QDateTime::currentDateTime().toMSecsSinceEpoch();
+        double dateTimeNow = QDateTime::currentDateTime().toMSecsSinceEpoch();
+        intError = Sub_FMInt->Cls_funManagerData_Modify(&dBVerify, &dataType, &userData, false);
+        emit addInfo(QString("Cls_funManagerData_Modify"), QStringLiteral(" 修改日期型数据"), QString("1"), QString::number(intError), (intError==1)?QStringLiteral("成功"):QStringLiteral("错误!!!"));
+
+        //获取日期型数据
+        dateTime = 0.0;
+        intSize = 0;
+        Cls_stuGetUserData getUserData(reinterpret_cast<void*&>(dateTime), intSize);
+        intError = Sub_FMInt->Cls_funManagerData_Extract(&dBVerify, &dataType, &fun, &getUserData);
+        QString strTime = QDateTime::fromMSecsSinceEpoch(dateTime).toString("yyyy-MM-dd hh:mm:ss zzz");
+        QString strTimeNow = QDateTime::fromMSecsSinceEpoch(dateTimeNow).toString("yyyy-MM-dd hh:mm:ss zzz");
+        emit addInfo(QString("Cls_funManagerData_Extract"), QStringLiteral(" 获取日期型数据"), strTimeNow, strTime, (intError==1)?QStringLiteral("成功"):QStringLiteral("错误!!!"));
+    }
+
+    {
+        //加入字符串型数据
+        string strString = "这是一段字符串abc";
+        Cls_stuDataType dataType(clsFileManager_intDataType_String, -1, "string");
+        Cls_stuUserData userData(&strString, 0);
+        intError = Sub_FMInt->Cls_funManagerData_Combine(&dBVerify, &dataType, nullptr, &userData, false, -1);
+        emit addInfo(QString("Cls_funManagerData_Combine"), QStringLiteral(" 加入字符串型数据"), QString("1"), QString::number(intError), (intError==1)?QStringLiteral("成功"):QStringLiteral("错误!!!"));
+
+        //修改字符串型数据
+        strString = "这是一段修改后的字符串abc";
+        intError = Sub_FMInt->Cls_funManagerData_Modify(&dBVerify, &dataType, &userData, false);
+        emit addInfo(QString("Cls_funManagerData_Modify"), QStringLiteral(" 修改字符串型数据"), QString("1"), QString::number(intError), (intError==1)?QStringLiteral("成功"):QStringLiteral("错误!!!"));
+
+        //获取字符串型数据
+        strString = "";
+        intSize = 0;
+        Cls_stuGetUserData getUserData(reinterpret_cast<void*&>(strString), intSize);
+        intError = Sub_FMInt->Cls_funManagerData_Extract(&dBVerify, &dataType, &fun, &getUserData);
+        emit addInfo(QString("Cls_funManagerData_Extract"), QStringLiteral(" 获取字符串型数据"), QString("这是一段修改后的字符串abc"), QString(strString.c_str()), (intError==1)?QStringLiteral("成功"):QStringLiteral("错误!!!"));
     }
 
 }
